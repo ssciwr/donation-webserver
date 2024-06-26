9b870f8ce8f73a2049433ffea05d5b07e97e7c0d
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2024-06-26 10:15:57 +0200

    add relational one-to-many, use sqlalchemy 2 syntax

--- src/website/models.py
+++ src/website/models.py
@@ -1,49 +1,59 @@
 from . import db
-
+from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
+from sqlalchemy import Integer, String, DateTime, ForeignKey
 # import hashlib
 from sqlalchemy.sql import func
+import datetime
+from typing import List
+
+class Base(DeclarativeBase):
+    pass
 
 
 # the raw data model
-class RawData(db.Model):
+class RawData(Base):
+    __tablename__ = "rawdata"
     # the submission id
-    id = db.Column(db.Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     # should this be the donated data as zip?
-    donation = db.Column(db.String(10000), nullable=False)
+    donation: Mapped[str] = mapped_column(String, nullable=False)
     # the hash checksum of the donation zip file, for example SHA-256
     # could also be SHA-3
     # Compute SHA-256 hash
     # sha256_hash = hashlib.sha256(data).hexdigest()
-    checksum = db.Column(db.String(100), nullable=False)
+    checksum: Mapped[str] = mapped_column(String, nullable=False)
     # Now the metadata
     # the date of the donation
-    date = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
+    date: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), 
+                                                    server_default=func.now(), nullable=False)
     # the email of the donor
-    email = db.Column(db.String(100), nullable=True)
+    email: Mapped[str] = mapped_column(String, nullable=True)
     # the age group of the donor in categories
-    age = db.Column(db.Integer, nullable=True)
+    age: Mapped[int] = mapped_column(Integer, nullable=True)
     # the region of the donor in categories
-    region = db.Column(db.String(100), nullable=True)
+    region: Mapped[int] = mapped_column(String, nullable=True)
     # the gender of the donor in categories
-    gender = db.Column(db.Integer, nullable=True)
+    gender: Mapped[int] = mapped_column(Integer, nullable=True)
     # if the emails are in the mother tongue of the donor
-    mother_tongue = db.Column(db.Integer, nullable=True)
+    mother_tongue: Mapped[int] = mapped_column(Integer, nullable=True)
     # the type of emails: formal, informal, etc. as categories
-    email_type = db.Column(db.Integer, nullable=True)
+    email_type: Mapped[int] = mapped_column(Integer, nullable=True)
     # set up the relationship with the processed data
-    # emails = db.relationship("ProcessedData")
+    # emails: Mapped[int] = relationship("ProcessedData", backref="RawData", lazy=True)
+    emails: Mapped[List["ProcessedData"]] = relationship()
 
 
-class ProcessedData(db.Model):
+class ProcessedData(Base):
+    __tablename__ = "processeddata"
     # the submission id
-    id = db.Column(db.Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     # the raw email text
-    raw_email = db.Column(db.String(100000), nullable=False)
+    raw_email: Mapped[str] = mapped_column(String, nullable=False)
     # the processed pseudonymized email text
-    processed_email = db.Column(db.String(100000), nullable=False)
+    processed_email: Mapped[str] = mapped_column(String, nullable=False)
     # the date of the processing
-    date = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
+    date: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), default=func.now(), nullable=False)
     # the language of the email
-    language = db.Column(db.String(10), nullable=False)
+    language: Mapped[str] = mapped_column(String, nullable=False)
     # the original donation id, one to many relationship
-    # donation_id = db.Column(db.Integer, db.ForeignKey("rawdata.id"), nullable=False)
+    donation_id: Mapped[int] = mapped_column(ForeignKey("rawdata.id"), nullable=True)
