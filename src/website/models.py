2689cbcfb1c4debd0a9484cbf516d3b34076e81e
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2024-07-02 10:22:17 +0200

    with metadata

--- src/website/models.py
+++ src/website/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
-from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, DateTime, ForeignKey
 
 # import hashlib
 from sqlalchemy.sql import func
@@ -11,34 +11,34 @@ from . import db
 class RawData(db.Model):
     # __tablename__ = "raw_data"
     # the submission id
-    id: Mapped[int] = mapped_column(primary_key=True)
+    donor_id: Mapped[int] = mapped_column(primary_key=True)
     # should this be the donated data as zip?
-    donation: Mapped[str] = mapped_column(String, nullable=False)
-    # # the hash checksum of the donation zip file, for example SHA-256
-    # # could also be SHA-3
-    # # Compute SHA-256 hash
-    # # sha256_hash = hashlib.sha256(data).hexdigest()
-    # checksum: Mapped[str] = mapped_column(String, nullable=False)
-    # # Now the metadata
-    # # the date of the donation
-    # date: Mapped[datetime.datetime] = mapped_column(
-    #     DateTime(timezone=True), server_default=func.now(), nullable=False
-    # )
-    # # the email of the donor
-    # email: Mapped[str] = mapped_column(String, nullable=True)
-    # # the age group of the donor in categories
-    # age: Mapped[int] = mapped_column(Integer, nullable=True)
-    # # the region of the donor in categories
-    # region: Mapped[int] = mapped_column(String, nullable=True)
-    # # the gender of the donor in categories
-    # gender: Mapped[int] = mapped_column(Integer, nullable=True)
-    # # if the emails are in the mother tongue of the donor
-    # mother_tongue: Mapped[int] = mapped_column(Integer, nullable=True)
-    # # the type of emails: formal, informal, etc. as categories
-    # email_type: Mapped[int] = mapped_column(Integer, nullable=True)
-    # # set up the relationship with the processed data
-    # # emails: Mapped[int] = relationship("ProcessedData", backref="RawData", lazy=True)
-    # # emails: Mapped[List["ProcessedData"]] = relationship()
+    donation: Mapped[str] = mapped_column(String, nullable=True)
+    # the hash checksum of the donation zip file, for example SHA-256
+    # could also be SHA-3
+    # Compute SHA-256 hash
+    # sha256_hash = hashlib.sha256(data).hexdigest()
+    checksum: Mapped[str] = mapped_column(String, nullable=True)
+    # Now the metadata
+    # the date of the donation
+    date: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    # the email of the donor
+    email: Mapped[str] = mapped_column(String, nullable=True)
+    # the age group of the donor in categories
+    age: Mapped[int] = mapped_column(Integer, nullable=True)
+    # the region of the donor in categories
+    region: Mapped[int] = mapped_column(String, nullable=True)
+    # the gender of the donor in categories
+    gender: Mapped[int] = mapped_column(Integer, nullable=True)
+    # if the emails are in the mother tongue of the donor
+    mother_tongue: Mapped[int] = mapped_column(Integer, nullable=True)
+    # the type of emails: formal, informal, etc. as categories
+    email_type: Mapped[int] = mapped_column(Integer, nullable=True)
+    # set up the relationship with the processed data
+    # emails: Mapped[int] = relationship("ProcessedData", backref="RawData", lazy=True)
+    # emails: Mapped[List["ProcessedData"]] = relationship()
 
 
 # class ProcessedData(Base):
