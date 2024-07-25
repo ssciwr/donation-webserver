30ee5e7735c4422d71530659de6ba48482f6f23f
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2024-07-25 15:23:04 +0200

    set up CI for mysql

--- src/app/website/__init__.py
+++ src/app/website/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-from os import path
+import os
 
 DB_NAME = "email_donations"
-
+PASSWD = os.getenv("MYSQL_PASSWORD")
 
 class Base(DeclarativeBase):
     pass
@@ -17,7 +17,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_prefixed_env()
     # reads the key from FLASK_SECRET_KEY env var
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://user:password@localhost/{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://donor:{PASSWD}@localhost/{DB_NAME}'
     db.init_app(app)
 
     from .views import views
