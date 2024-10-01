86ef203fdc0b2d58ff3723f67d3d446a53f1177e
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2024-10-01 14:57:29 +0200

    separate backend and frontend part 1

--- src/app/main.py
+++ src/app/main.py
@@ -1,13 +1,6 @@
-from flask import send_from_directory
-from website import create_app
+from backend import create_app
 
 app = create_app()
-
-
-# Path for our main Svelte page
-@app.route("/")
-def client():
-    return send_from_directory('client/public', 'index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
