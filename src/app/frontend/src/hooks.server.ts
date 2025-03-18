1a0bf233f8175c12e05fc706d424c6d53b1858b8
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2025-03-18 08:49:43 +0100

    disable hooks, set db properties from dotenv

--- src/app/frontend/src/hooks.server.ts
+++ src/app/frontend/src/hooks.server.ts
@@ -1,6 +1,6 @@
 
-import { db } from '$lib/server/db';
-import type { Handle } from '@sveltejs/kit';
+// import { db } from '$lib/server/db';
+// import type { Handle } from '@sveltejs/kit';
 
 
 // export const handle: Handle = ({ event, resolve }) => {
@@ -10,10 +10,10 @@ import type { Handle } from '@sveltejs/kit';
 // };
 
 
-export const handle = async ({event, resolve}) => {
-	const dbConn = await db();
-	event.locals.dbConn = dbConn;
-	const response = await resolve(event);
-	dbConn.release();
-	return response;
-}
+// export const handle = async ({event, resolve}) => {
+// 	const dbConn = await db();
+// 	event.locals.dbConn = dbConn;
+// 	const response = await resolve(event);
+// 	dbConn.release();
+// 	return response;
+// }
--- src/app/frontend/src/lib/server/db.ts
+++ src/app/frontend/src/lib/server/db.ts
@@ -1,15 +1,19 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
+import dotenv from 'dotenv';
 
+dotenv.config();
 //const db = drizzle(process.env.DATABASE_URL);
 
 // we will use client connections since we do not expect many concurrent connections
 // and connection also does not need to be maintained for long
 const connection = await mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "donor",
-  database: "email_donations",
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : 3306,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
 });
-const db = drizzle({ client: connection });
+
+export const db = drizzle(connection);
 
