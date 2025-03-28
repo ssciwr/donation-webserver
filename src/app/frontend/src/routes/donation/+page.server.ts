4f7ce1f3792fec3070060d1f5a922828fe6ae088
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2025-03-28 11:48:31 +0100

    set autoclose to false to submit data

---
+++ src/app/frontend/src/routes/donation/+page.server.ts
@@ -0,0 +1,26 @@
+import { db } from '$lib/server/db';
+import type { Actions, PageServerLoad } from './$types';
+import { donationsTable } from '$lib/server/schema';
+export const prerender = false;
+
+
+
+export const load: PageServerLoad = async () => {
+    const users = await db.select().from(donationsTable);
+    console.log('Getting all users from the database: ', users);
+
+    return {
+        users
+    };
+};
+
+
+export const actions: Actions = {
+    default: async ({ request }) => {
+        const data = await request.formData();
+        const id = Number(data.get('id'));
+        await db.insert(donationsTable).values({
+            id
+        });
+    }
+};
