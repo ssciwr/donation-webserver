b504f797bf5bfacd0f385cef34cd8c9a8601790a
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2025-04-28 14:33:27 +0200

    update playwright config

--- src/app/frontend/playwright.config.ts
+++ src/app/frontend/playwright.config.ts
@@ -5,7 +5,8 @@ const config: PlaywrightTestConfig = {
 		// command: 'pnpm run build && pnpm run preview',
 		// running the tests in dev to avoid db connection issues
 		command: 'pnpm run dev',
-		port: 5173
+		port: 5173,
+		reuseExistingServer:true
 	},
 	testDir: './tests'
 };
