314ae55ee3aac114fda421660c4ed6df3690cd05
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2025-04-28 15:26:16 +0200

    update content home and about

--- src/app/frontend/src/routes/+page.svelte
+++ src/app/frontend/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import './global.css';
+  import { Button } from 'flowbite-svelte';
   import { goto } from '$app/navigation';
 
   function gotoDonate() {
@@ -8,15 +9,27 @@
 
 </script>
 
-<h1 class="mb-4 font-extrabold center leading-none tracking-tight">Projekt MailCom</h1>
-
-<main class='p-8 h-auto'>
-  
-    <article class="section" id="tutorial"><h2>Tutorial</h2></article>
-    <article class="section" id="b"><h2>Information</h2></article>
-    <article class="section" id="c"><h2>FAQ</h2></article>
-    <article class="section" id="d"><h2>Blog</h2></article>
-  
-</main>
+<h1 class="mb-4 font-extrabold center leading-none tracking-tight text-gray-100 text-4xl">Projekt MailCom</h1>
 
 
+
+
+ <a href="/about">
+<figure class="max-w-lg">
+<img class="h-auto max-w-full object-cover rounded-lg shadow-lg" 
+     src="/images/pexels-suzyhazelwood-1768060.jpg" 
+     alt="A stack of letters" 
+     
+/>
+<figcaption class="mt-2 text-sm text-center text-gray-100">Photo by Suzy Hazelwood: https://www.pexels.com/photo/pile-of-letters-in-envelopes-1768060/"</figcaption>
+</figure>
+</a>
+
+<a href="/donation">
+  <p class="text-gray-100">Hier gehts zur Email Spende:
+  <Button class="w-fit" on:click={gotoDonate}>
+    Emails spenden
+</Button>
+</p>   
+
+</a>
--- src/app/frontend/src/routes/about/+page.svelte
+++ src/app/frontend/src/routes/about/+page.svelte
@@ -1,19 +1,9 @@
 <script lang="ts">
-    import { Button, Modal, Label, Input, Checkbox } from 'flowbite-svelte'
+    import { Button } from 'flowbite-svelte'
     import { goto } from '$app/navigation';
-    let emailModal: boolean = $state(false);
-    let subscribe: boolean = $state(false);
-    let email: string = $state('');
   
     function gotoDonate() {
       goto('/donation');
-    };
-
-    let check_signup = () => {
-        if (!subscribe) {
-            alert('Bitte bestaetigen Sie die Einwilligung zum Newsletter um fortzufahren.');
-            emailModal = true; 
-        }
     };
 
   </script>
@@ -26,42 +16,36 @@
         überschaubare Anzahl an umfassenden linguistischen Studien, liegt in der mangelnden Datenbasis. Um E-Mails analysieren zu können, 
         benötigen wir E-Mails als Korpora. Anders als bei Briefen, von denen ein Teil bis heute in sehr verschiedenen kleinen oder großen 
         Archiven aufbewahrt und dort zur Analyse genutzt werden können, haben wir für E-Mails keine Datenbanken, die uns ausreichend E-Mails 
-        zur Analyse zur Verfügung stellen könnten.  Wir sind demzufolge auf E-Mail-Spenden angewiesen.  
+        zur Analyse zur Verfügung stellen könnten.  Wir sind demzufolge auf E-Mail-Spenden angewiesen.
         </p>
+        <br>
     <p class="text-gray-100">Mit den gespendeten E-Mails, die selbstverständlich von uns komplett anonymisiert werden, hätten wir die Möglichkeit, 
         verschiedene linguistische Fragestellungen zu bearbeiten oder auch die Frage zu beantworten, ob und warum wir E-Mails anders 
         schreiben als Briefe.
     </p>
-
-    <h3 class="text-gray-100">
+    <br>
+    <h3 class="text-gray-100 text-xl">
         Umgang mit den Daten/Anonymisierung
     </h3>
     <p class="text-gray-100"> Hinweise an die Spendenden: Bitte spenden Sie ausschließlich E-Mails, die Sie selbst gesendet haben oder bei denen 
         Sie die Zustimmung aller Beteiligten eingeholt haben.  </p>
-    <p class="text-gray-100">Anonymisierung der Mails: Die E-Mails werden automatisch anonymisiert. Dazu nutzen wir ein entsprechendes Tool, 
+        <br>
+        <h3 class="text-gray-100 text-xl">
+            Anonymisierung der Mails
+        </h3>
+    <p class="text-gray-100">Die E-Mails werden automatisch anonymisiert. Dazu nutzen wir ein entsprechendes Tool, 
         welches wir im vergangenen Jahr an hunderten Test-E-Mails überprüft haben. Durch die Anonymisierung werden alle persönlichen Daten wie Namen, 
         Telefonnummer oder Adressen aus dem E-Mails herausgestrichen und durch Platzhalter wie Name für Sabine Mustermann ersetzt. 
         </p>
-    <p class="text-gray-100"> Anzahl und Art der E-Mails: Es gibt keine Begrenzung. Sie können grundsätzlich so viele E-Mails spenden wie Sie möchten. 
+        <br>
+        <h3 class="text-gray-100 text-xl">
+            Anzahl und Art der E-Mails
+        </h3>
+    <p class="text-gray-100">Es gibt keine Begrenzung. Sie können grundsätzlich so viele E-Mails spenden wie Sie möchten. 
         Die E-Mails können gerne zu den unterschiedlichsten Anlässen und in unterschiedlichsten Situationen geschrieben worden sein. 
         </p> 
+        <br>
     <Button class="w-fit" on:click={gotoDonate}>
         Emails spenden
     </Button>
-    <Button on:click={() => (emailModal = true)}>Projektneuigkeiten erhalten</Button>
 </main>
-
-<Modal bind:open={emailModal} size="xs" autoclose={false} class="w-full">
-    <form class="flex flex-col space-y-6" action="#" method="POST">
-      <Label class="space-y-2">
-          <span>Falls Sie unseren Newsletter und Projektneuigkeiten erhalten möchten, 
-            geben Sie bitte Ihre Email Adresse an:</span>
-        <Input type="email" name="email" bind:value={email} placeholder="name@company.com" required />
-      </Label>
-      <div class="flex items-start">
-        <Checkbox bind:checked={subscribe}>Ich möchte den Newsletter und Projektmitteilungen erhalten.</Checkbox>
-      </div>
-      <Button type="submit" class="w-full1" on:click={() => check_signup()}>Projektmitteilungen erhalten</Button>
-      <Button class="w-full1">Keine Benachrichtigungen erhalten</Button>
-    </form>
-  </Modal>
