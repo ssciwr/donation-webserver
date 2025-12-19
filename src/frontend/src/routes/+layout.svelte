7d532eabd64a51f44f9b0f13a411dcd21ab314db
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2025-12-19 09:00:14 +0100

    fix: update title passing in layout

--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -16,6 +16,10 @@
   }
 </script>
 
+<svelte:head>
+  <title>Mailcom</title>
+</svelte:head>
+
 
 <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 relative">
   <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
