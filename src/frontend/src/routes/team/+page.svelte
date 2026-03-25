9ba3363c4ddc3f2500c87a3f2e0b3108dbea3895
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2026-03-25 14:08:55 +0100

    fix: some minor typos, changes

--- src/frontend/src/routes/team/+page.svelte
+++ src/frontend/src/routes/team/+page.svelte
@@ -15,64 +15,64 @@
 	<br>
 
 	<h3 class="text-xl text-center">
-        {$t.team.ResearchTeam}
+        {$t.team.researchTeam}
     </h3>
-	<h4 class="text-l text-center italic">
-        {$t.team.ResearchTeamSubHeading1}
+	<h4 class="text-center italic">
+        {$t.team.researchTeamSubHeading1}
     </h4>
-	<P class="text-left text-gray-700 mt-4" weight="bold" >{$t.team.PhDStudent1}</P>
-	<P class="text-left text-gray-700 mt-4">{$t.team.PhDStudent1Description}</P>
+	<P class="text-left text-gray-700 mt-4" weight="bold" >{$t.team.phDStudent1}</P>
+	<P class="text-left text-gray-700 mt-4">{$t.team.phDStudent1Description}</P>
 	<div style="height: 4px;"></div>
 	<Button class="w-fit bg-red-900 xs" pill href="https://www.hcts.uni-heidelberg.de/en/centre/people-a-z/laura-gartner">
 		<ArrowRightOutline class="h-6 w-6" />
-        {$t.team.PhDStudent1LinkText}
+        {$t.team.phDStudent1LinkText}
     </Button>
 	<br>
-	<h4 class="text-l text-center italic">
-        {$t.team.ResearchTeamSubHeading2}
+	<h4 class="text-center italic">
+        {$t.team.researchTeamSubHeading2}
     </h4>
-    <List class="text-left text-gray-700 mt-4 circle-list">
-      <Li> {$t.team.StudentAssistant1}</Li>
-      <Li> {$t.team.StudentAssistant2}</Li>
-      <Li> {$t.team.StudentAssistant3}</Li>
+    <List class="text-left text-gray-700 mt-4">
+      <Li> {$t.team.studentAssistant1}</Li>
+      <Li> {$t.team.studentAssistant2}</Li>
+      <Li> {$t.team.studentAssistant3}</Li>
     </List>
 	<br>
-	<h4 class="text-l text-center italic">
-        {$t.team.ResearchTeamSubHeading3}
+	<h4 class="text-center italic">
+        {$t.team.researchTeamSubHeading3}
     </h4>
-	<P class="text-left text-gray-700 mt-4" weight="bold" >{$t.team.TeamLead}</P>
-	<P class="text-left text-gray-700 mt-4">{$t.team.TeamLeadDescription}</P>
+	<P class="text-left text-gray-700 mt-4" weight="bold" >{$t.team.teamLead}</P>
+	<P class="text-left text-gray-700 mt-4">{$t.team.teamLeadDescription}</P>
 	<div style="height: 4px;"></div>
 	<Button class="w-fit bg-red-900 xs" pill href="https://www.uni-heidelberg.de/rose/personen/grosse.html">
 		<ArrowRightOutline class="h-6 w-6" />
-        {$t.team.TeamLeadLinkText}
+        {$t.team.teamLeadLinkText}
     </Button>
 	<br>
 	<h3 class="text-xl text-center">
-        {$t.team.RSETeam}
+        {$t.team.rSETeam}
     </h3>
-	<List class="text-left text-gray-700 mt-4 circle-list">
-      <Li> {$t.team.RSETeam1}</Li>
-      <Li> {$t.team.RSETeam2}</Li>
+	<List class="text-left text-gray-700 mt-4">
+      <Li> {$t.team.rSETeam1}</Li>
+      <Li> {$t.team.rSETeam2}</Li>
     </List>
 	<div style="height: 4px;"></div>
 	<Button class="w-fit bg-red-900 xs" pill href="https://www.ssc.uni-heidelberg.de">
 		<ArrowRightOutline class="h-6 w-6" />
-        {$t.team.RSELinkText}
+        {$t.team.rSELinkText}
     </Button>
 	<br>
-	<P class="text-left text-gray-700 mt-4" weight="bold" >{$t.team.FundingInfo}</P>
+	<P class="text-left text-gray-700 mt-4" weight="bold" >{$t.team.fundingInfo}</P>
 	<br>
-	<h4 class="text-l text-center italic">
-        {$t.team.Publications}
+	<h4 class="text-center italic">
+        {$t.team.publications}
     </h4>
-	<List class="text-left text-gray-700 mt-4 circle-list">
-      <Li> {$t.team.Publication1} 
+	<List class="text-left text-gray-700 mt-4">
+      <Li> {$t.team.publication1} 
 	  <a href="https://doi.org/10.11588/heibooks.1288.c18070" target="_blank" rel="noreferrer" 
-          class="text-primary-900 hover:underline">[{$t.team.DOIText}]</a> </Li>
-      <Li> {$t.team.Publication2} 
+          class="text-primary-900 hover:underline">[{$t.team.dOIText}]</a> </Li>
+      <Li> {$t.team.publication2} 
 	  <a href="https://dx.doi.org/10.2139/ssrn.6083118" target="_blank" rel="noreferrer" 
-          class="text-primary-900 hover:underline">[{$t.team.DOIText}]</a> </Li>
+          class="text-primary-900 hover:underline">[{$t.team.dOIText}]</a> </Li>
     </List>
 	<br>
 </div>
